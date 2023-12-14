f6c7fba0b0b4eb89e11ca28dd6f509aaeef40d32
Mohamed Eyad <mohamed.eyad2612@gmail.com>
2023-12-14 15:58:37 +0200

    reduced test set size

--- src/dataset.py
+++ src/dataset.py
@@ -652,9 +652,9 @@ class FewShotDataset(torch.utils.data.Dataset):
 
         if verbose:
             logger.info("*** Example ***")
-            # logger.info("guid: %s" % (example.guid))
-            # logger.info("features: %s" % features)
-            # logger.info("text: %s" % self.tokenizer.decode(features.input_ids))
+            logger.info("guid: %s" % (example.guid))
+            logger.info("features: %s" % features)
+            logger.info("text: %s" % self.tokenizer.decode(features.input_ids))
 
         return features
 
