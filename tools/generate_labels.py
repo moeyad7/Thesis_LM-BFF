ed092d22249c30ff71c53e25704348afba697c98
Mohamed Eyad <mohamed.eyad2612@gmail.com>
2023-12-22 17:50:44 +0200

    removed variable that causes error

--- tools/generate_labels.py
+++ tools/generate_labels.py
@@ -211,7 +211,7 @@ def main():
     # For BERT, increase the size of the segment (token type) embeddings
     if config.model_type == 'bert':
         model.resize_token_embeddings(len(tokenizer))
-        resize_token_type_embeddings(model, new_num_types=10, random_segment=model_args.random_segment)
+        resize_token_type_embeddings(model, new_num_types=10)
 
     # Pass dataset and argument information to the model
     model.model_args = model_args
