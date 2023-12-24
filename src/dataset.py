23091a22c6e813961f59caf660c3b9457fe28ad8
Mohamed Eyad <mohamed.eyad2612@gmail.com>
2023-12-24 17:08:02 +0200

    decreased number of prints

--- src/dataset.py
+++ src/dataset.py
@@ -42,25 +42,19 @@ class OurInputFeatures(InputFeatures):
 
 def input_example_to_string(example, sep_token): 
     if example.text_b is None:
-        print("I am at line 45 dataset.py")
         return example.text_a
     else:
-        print("I am at line 48 dataset.py")
         # Warning: very simple hack here
         return example.text_a + ' ' + sep_token + ' ' + example.text_b
 
 def input_example_to_tuple(example): 
     if example.text_b is None:
-        print("I am at line 54 dataset.py")
         if pd.isna(example.text_a) or example.text_a is None:
-            print("I am at line 56 dataset.py")
             return ['']
             logger.warn("Empty input")
         else:
-            print("I am at line 60 dataset.py")
             return [example.text_a]
     else:
-        print("I am at line 63 dataset.py")
         return [example.text_a, example.text_b]
 
 def tokenize_multipart_input(
@@ -86,7 +80,6 @@ def tokenize_multipart_input(
     mask_pos = None # Position of the mask token
 
     if prompt:
-        print("I am at line 90 dataset.py")
         """
         Concatenate all sentences and prompts based on the provided template.
         Template example: '*cls*It was*mask*.*sent_0**<sep>*label_0:*sent_1**<sep>**label_1*:*sent_2**<sep>*'
@@ -234,7 +227,6 @@ def tokenize_multipart_input(
                 # Do not have text_b
                 continue
             if pd.isna(input_text) or input_text is None:
-                print("I am at line 239 dataset.py")
                 # Empty input
                 input_text = ''
             input_tokens = enc(input_text) + [tokenizer.sep_token_id]
@@ -243,14 +235,12 @@ def tokenize_multipart_input(
             token_type_ids += [sent_id for i in range(len(input_tokens))]
 
         if 'T5' in type(tokenizer).__name__: # T5 does not have CLS token
-            print("I am at line 248 dataset.py")
             input_ids = input_ids[1:]
             attention_mask = attention_mask[1:]
             token_type_ids = token_type_ids[1:]
 
     # Padding
     if first_sent_limit is not None and len(input_ids) > max_length:
-        print("I am at line 255 dataset.py")
         # If using sentence limit, the total length still exceeds the maximum limit, report a warning
         logger.warn("Input exceeds max_length limit ({}): {}".format(len(input_ids), tokenizer.decode(input_ids)))
 
@@ -262,14 +252,11 @@ def tokenize_multipart_input(
 
     # Truncate
     if len(input_ids) > max_length:
-        print("I am at line 268 dataset.py")
         if truncate_head:
-            print("I am at line 270 dataset.py")
             input_ids = input_ids[-max_length:]
             attention_mask = attention_mask[-max_length:]
             token_type_ids = token_type_ids[-max_length:]
         else:
-            print("I am at line 275 dataset.py")
             # Default is to truncate the tail
             input_ids = input_ids[:max_length]
             attention_mask = attention_mask[:max_length]
@@ -278,7 +265,6 @@ def tokenize_multipart_input(
 
     # Find mask token
     if prompt:
-        print("I am at line 284 dataset.py")
         # print(tokenizer.decode(input_ids))
         mask_pos = [input_ids.index(tokenizer.mask_token_id)]
         # Make sure that the masked position is inside the max_length
@@ -310,7 +296,6 @@ class FewShotDataset(torch.utils.data.Dataset):
         # If not using demonstrations, use use_demo=True
         self.use_demo = use_demo
         if self.use_demo:
-            print("I am at line 317 dataset.py")
             logger.info("Use demonstrations")
         assert mode in ["train", "dev", "test"]
 
@@ -318,7 +303,6 @@ class FewShotDataset(torch.utils.data.Dataset):
         self.label_list = self.processor.get_labels()
         self.num_labels = len(self.label_list)
         if args.prompt:
-            print("I am at line 325 dataset.py")
             assert args.mapping is not None
             self.label_to_word = eval(args.mapping)
 
@@ -338,7 +322,6 @@ class FewShotDataset(torch.utils.data.Dataset):
                 print("I am at line 342 dataset.py")
                 self.label_word_list = [self.label_to_word[label] for label in self.label_list]
             else:
-                print("I am at line 345 dataset.py")
                 # Regression task
                 # '0' represents low polarity and '1' represents high polarity.
                 self.label_word_list = [self.label_to_word[label] for label in ['0', '1']]
@@ -350,11 +333,9 @@ class FewShotDataset(torch.utils.data.Dataset):
         # Multiple sampling: when using demonstrations, we sample different combinations of demonstrations during 
         # inference and aggregate the results by averaging the logits. The number of different samples is num_sample.
         if (mode == "train") or not self.use_demo:
-            print("I am at line 357 dataset.py")
             # We do not do multiple sampling when not using demonstrations or when it's the training mode 
             self.num_sample = 1    
         else:
-            print("I am at line 361 dataset.py")
             self.num_sample = args.num_sample
 
         # If we use multiple templates, we also need to do multiple sampling during inference.
@@ -383,27 +364,22 @@ class FewShotDataset(torch.utils.data.Dataset):
         with FileLock(lock_path):
 
             if os.path.exists(cached_features_file) and not args.overwrite_cache:
-                print("I am at line 390 dataset.py")
                 start = time.time()
                 self.support_examples, self.query_examples = torch.load(cached_features_file)
                 logger.info(
                     f"Loading features from cached file {cached_features_file} [took %.3f s]", time.time() - start
                 )
             else:
-                print("I am at line 397 dataset.py")
                 logger.info(f"Creating features from dataset file at {args.data_dir}")
 
                 # The support examples are sourced from the training set.
                 self.support_examples = self.processor.get_train_examples(args.data_dir)
 
                 if mode == "dev":
-                    print("I am at line 404 dataset.py")
                     self.query_examples = self.processor.get_dev_examples(args.data_dir)
                 elif mode == "test":
-                    print("I am at line 407 dataset.py")
                     self.query_examples = self.processor.get_test_examples(args.data_dir)
                 else:
-                    print("I am at line 410 dataset.py")
                     self.query_examples = self.support_examples
 
                 start = time.time()
@@ -415,13 +391,10 @@ class FewShotDataset(torch.utils.data.Dataset):
 
         # For filtering in using demonstrations, load pre-calculated embeddings
         if self.use_demo and args.demo_filter:
-            print("I am at line 422 dataset.py")
             split_name = ''
             if mode == 'train':
-                print("I am at line 425 dataset.py")
                 split_name = 'train'
             elif mode == 'dev':
-                print("I am at line 428 dataset.py")
                 if args.task_name == 'mnli':
                     split_name = 'dev_matched'
                 elif args.task_name == 'mnli-mm':
@@ -429,7 +402,6 @@ class FewShotDataset(torch.utils.data.Dataset):
                 else:
                     split_name = 'dev'
             elif mode == 'test':
-                print("I am at line 436 dataset.py")
                 if args.task_name == 'mnli':
                     split_name = 'test_matched'
                 elif args.task_name == 'mnli-mm':
@@ -456,7 +428,6 @@ class FewShotDataset(torch.utils.data.Dataset):
             for query_idx in range(len(self.query_examples)):
                 # If training, exclude the current example. Else keep all.
                 if self.use_demo and args.demo_filter:
-                    print("I am at line 463 dataset.py")
                     # Demonstration filtering
                     candidate = [support_idx for support_idx in support_indices
                                    if support_idx != query_idx or mode != "train"]
@@ -465,42 +436,33 @@ class FewShotDataset(torch.utils.data.Dataset):
                         sim_score.append((support_idx, util.pytorch_cos_sim(self.support_emb[support_idx], self.query_emb[query_idx])))
                     sim_score.sort(key=lambda x: x[1], reverse=True)
                     if self.num_labels == 1:
-                        print("I am at line 472 dataset.py")
                         # Regression task
                         limit_each_label = int(len(sim_score) // 2 * args.demo_filter_rate)
                         count_each_label = {'0': 0, '1': 0}
                         context_indices = []
 
                         if args.debug_mode:
-                            print("I am at line 479 dataset.py")
                             print("Query %s: %s" % (self.query_examples[query_idx].label, self.query_examples[query_idx].text_a)) # debug
                         for support_idx, score in sim_score:
                             if count_each_label['0' if float(self.support_examples[support_idx].label) <= median_mapping[args.task_name] else '1'] < limit_each_label:
-                                print("I am at line 483 dataset.py")
                                 count_each_label['0' if float(self.support_examples[support_idx].label) <= median_mapping[args.task_name] else '1'] += 1
                                 context_indices.append(support_idx)
                                 if args.debug_mode:
-                                    print("I am at line 487 dataset.py")
                                     print("    %.4f %s | %s" % (score, self.support_examples[support_idx].label, self.support_examples[support_idx].text_a)) # debug
                     else:
-                        print("I am at line 490 dataset.py")
                         limit_each_label = int(len(sim_score) // self.num_labels * args.demo_filter_rate)
                         count_each_label = {label: 0 for label in self.label_list}
                         context_indices = []
 
                         if args.debug_mode:
-                            print("I am at line 496 dataset.py")
                             print("Query %s: %s" % (self.query_examples[query_idx].label, self.query_examples[query_idx].text_a)) # debug
                         for support_idx, score in sim_score:
                             if count_each_label[self.support_examples[support_idx].label] < limit_each_label:
-                                print("I am at line 500 dataset.py")
                                 count_each_label[self.support_examples[support_idx].label] += 1
                                 context_indices.append(support_idx)
                                 if args.debug_mode:
-                                    print("I am at line 504 dataset.py")
                                     print("    %.4f %s | %s" % (score, self.support_examples[support_idx].label, self.support_examples[support_idx].text_a)) # debug
                 else:
-                    print("I am at line 507 dataset.py")
                     # Using demonstrations without filtering
                     context_indices = [support_idx for support_idx in support_indices
                                if support_idx != query_idx or mode != "train"]
@@ -510,7 +472,6 @@ class FewShotDataset(torch.utils.data.Dataset):
 
         # If it is not training, we pre-process the data; otherwise, we process the data online.
         if mode != "train":
-            print("I am at line 517 dataset.py")
             self.features = []
             _ = 0
             for query_idx, context_indices, bootstrap_idx in self.example_idx:
@@ -520,10 +481,8 @@ class FewShotDataset(torch.utils.data.Dataset):
                 supports = self.select_context([self.support_examples[i] for i in context_indices])
 
                 if args.template_list is not None:
-                    print("I am at line 527 dataset.py")
                     template = args.template_list[sample_idx % len(args.template_list)] # Use template in order
                 else:
-                    print("I am at line 530 dataset.py")
                     template = args.template
                     
 
@@ -540,7 +499,6 @@ class FewShotDataset(torch.utils.data.Dataset):
 
                 _ += 1
         else:
-            print("I am at line 547 dataset.py")
             self.features = None
 
     def select_context(self, context_examples):
@@ -550,19 +508,16 @@ class FewShotDataset(torch.utils.data.Dataset):
         max_demo_per_label = 1
         counts = {k: 0 for k in self.label_list}
         if len(self.label_list) == 1:
-            print("I am at line 557 dataset.py")
             # Regression
             counts = {'0': 0, '1': 0}
         selection = []
 
         if self.args.gpt3_in_context_head or self.args.gpt3_in_context_tail:
-            print("I am at line 563 dataset.py")
             # For GPT-3's in-context learning, we sample gpt3_in_context_num demonstrations randomly. 
             order = np.random.permutation(len(context_examples))
             for i in range(min(self.args.gpt3_in_context_num, len(order))):
                 selection.append(context_examples[order[i]])
         else:
-            print("I am at line 569 dataset.py")
             # Our sampling strategy
             order = np.random.permutation(len(context_examples))
 
@@ -586,7 +541,6 @@ class FewShotDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, i):
         if self.features is None:
-            print("I am at line 596 dataset.py")
             query_idx, context_indices, bootstrap_idx = self.example_idx[i]
             # The input (query) example
             example = self.query_examples[query_idx]
@@ -594,10 +548,8 @@ class FewShotDataset(torch.utils.data.Dataset):
             supports = self.select_context([self.support_examples[i] for i in context_indices])
 
             if self.args.template_list is not None:
-                print("I am at line 604 dataset.py")
                 template = self.args.template_list[sample_idx % len(self.args.template_list)]
             else:
-                print("I am at line 607 dataset.py")
                 template = self.args.template
 
             features = self.convert_fn(
@@ -611,7 +563,6 @@ class FewShotDataset(torch.utils.data.Dataset):
                 verbose=False,
             )
         else:
-            print("I am at line 621 dataset.py")
             features = self.features[i]
             
         return features
@@ -639,25 +590,20 @@ class FewShotDataset(torch.utils.data.Dataset):
         # Prepare labels
         label_map = {label: i for i, label in enumerate(label_list)} # Mapping the label names to label ids
         if len(label_list) == 1:
-            print("I am at line 649 dataset.py")
             # Regression
             label_map = {'0': 0, '1': 1}
 
         # Get example's label id (for training/inference)
         if example.label is None:
-            print("I am at line 655 dataset.py")
             example_label = None
         elif len(label_list) == 1:
-            print("I am at line 658 dataset.py")
             # Regerssion
             example_label = float(example.label)
         else:
-            print("I am at line 662 dataset.py")
             example_label = label_map[example.label]
 
         # Prepare other features
         if not use_demo:
-            print("I am at line 667 dataset.py")
             # No using demonstrations
             inputs = tokenize_multipart_input(
                 input_text_list=input_example_to_tuple(example),
@@ -674,17 +620,14 @@ class FewShotDataset(torch.utils.data.Dataset):
             features = OurInputFeatures(**inputs, label=example_label)
 
         else:
-            print("I am at line 684 dataset.py")
             # Using demonstrations
 
             # Max length
             if self.args.double_demo:
-                print("I am at line 689 dataset.py")
                 # When using demonstrations, double the maximum length
                 # Note that in this case, args.max_seq_length is the maximum length for a single sentence
                 max_length = max_length * 2
             if self.args.gpt3_in_context_head or self.args.gpt3_in_context_tail:
-                print("I am at line 694 dataset.py")
                 # When using GPT-3's in-context learning, take the maximum tokenization length of the model (512)
                 max_length = 512
 
@@ -697,27 +640,22 @@ class FewShotDataset(torch.utils.data.Dataset):
             support_by_label = [[] for i in range(len(label_map))]
 
             if self.args.gpt3_in_context_head or self.args.gpt3_in_context_tail:
-                print("I am at line 707 dataset.py")
                 support_labels = []
                 augmented_example = query_text
                 for support_example in supports:
                     augmented_example += input_example_to_tuple(support_example)
                     current_label = support_example.label
                     if len(label_list) == 1:
-                        print("I am at line 714 dataset.py")
                         current_label = '0' if float(current_label) <= median_mapping[self.args.task_name] else '1' # Regression
                     support_labels.append(label_map[current_label])
             else:
-                print("I am at line 718 dataset.py")
                 # Group support examples by label
                 for label_name, label_id in label_map.items():
                     if len(label_list) == 1:
-                        print("I am at line 718 dataset.py")
                         # Regression
                         for support_example in filter(lambda s: ('0' if float(s.label) <= median_mapping[self.args.task_name] else '1') == label_name, supports):
                             support_by_label[label_id] += input_example_to_tuple(support_example)
                     else:
-                        print("I am at line 727 dataset.py")
                         for support_example in filter(lambda s: s.label == label_name, supports):
                             support_by_label[label_id] += input_example_to_tuple(support_example)
 
